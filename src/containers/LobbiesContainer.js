ff396a2af7c6954992ff5554e2ee41f195f790c7
branmar97 <brandonmar97@gmail.com>
2021-03-10 14:33:03 -0500

    Setup Lobbies Container

---
+++ src/containers/LobbiesContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchLobbies } from '../actions/lobbyActions';
+import Lobbies from '../components/Lobbies';
+
+class LobbiesContainer extends Component {
+   
+    render() { 
+        return ( 
+            <div>
+
+            </div>
+         );
+    }
+}
+ 
+export default LobbiesContainer;
