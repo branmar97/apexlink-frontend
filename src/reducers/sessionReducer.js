2e45bf7a7c40b96a0283877e891f35091ace8741
branmar97 <brandonmar97@gmail.com>
2021-03-10 13:05:37 -0500

    Updated sessions reducer

--- src/reducers/sessionReducer.js
+++ src/reducers/sessionReducer.js
@@ -1,5 +1,20 @@
-export default function manageSessions(state = {
-    sessions: [],
-}, action) {
-    
+const manageSessions = (state = { sessions: [], loading: false }, action) => {
+    switch(action.type) {
+        case 'LOADING_SESSIONS':
+            return {
+                ...state,
+                sessions: [...state.sessions],
+                loading: true
+            }
+        case 'ADD_SESSIONS':
+            return {
+                ...state,
+                sessions: action.sessions,
+                loading: false
+            }
+        default:
+            return state;
+    }
 }
+
+export default manageSessions;
