cd2698bba1c2f874bcda2cc3c2244e1b468db24c
branmar97 <brandonmar97@gmail.com>
2021-03-11 14:34:33 -0500

    Built new lobby form

--- src/components/LobbyForm.js
+++ src/components/LobbyForm.js
@@ -21,7 +21,95 @@ class LobbyForm extends Component {
     }
 
     render() { 
-        return (  );
+        return ( 
+            <div>
+                <form>
+                    <label htmlFor='gamertagText'>Gamertag</label><br />
+                    
+                    <input 
+                    type='text' 
+                    name='gamertagText'
+                    value={this.state.gamertagText}
+                    onChange={this.handleOnChange}
+                    placeholder='ex. EliteGamer28'
+                    />
+                    <br /><br />
+
+                    <label htmlFor='micBool'>Mic required?</label>
+
+                    <input
+                    type='checkbox'
+                    name='micBool'
+                    defaultChecked={this.state.micBool}
+                    onChange={this.handleOnChange}
+                    />
+                    <br /><br />
+
+                    <label htmlFor='regionText'>Region</label><br />
+
+                    <select 
+                    name='regionText'
+                    value={this.state.regionText}
+                    onChange={this.handleOnChange}
+                    >
+                        <option value='United States'>United States</option>
+                        <option value='Canada'>Canada</option>
+                        <option value='United Kingdom'>United Kingdom</option>
+                    </select>
+                    <br /><br />
+
+                    <label htmlFor='platformText'>Platform</label><br />
+
+                    <select 
+                    name='platformText'
+                    value={this.state.platformText}
+                    onChange={this.handleOnChange}
+                    >
+                        <option value='PC'>PC</option>
+                        <option value='PC'>Xbox</option>
+                        <option value='PC'>Playstation</option>
+                    </select>
+                    <br /><br />
+
+                    <label htmlFor='gamemodeText'>Gamemode</label><br />
+
+                    <select 
+                    name='gamemodeText'
+                    value={this.state.gamemodeText}
+                    onChange={this.handleOnChange}
+                    >
+                        <option value='Duos'>Duos</option>
+                        <option value='Trios'>Trios</option>
+                        <option value='Ranked'>Ranked</option>
+                    </select>
+                    <br /><br />
+
+                    <label htmlFor='skillText'>Skill</label><br />
+
+                    <select 
+                    name='skillText'
+                    value={this.state.skillText}
+                    onChange={this.handleOnChange}
+                    >
+                        <option value='Beginner'>Beginner</option>
+                        <option value='Intermediate'>Intermediate</option>
+                        <option value='Experienced'>Experienced</option>
+                    </select>
+                    <br /><br />
+
+                    <label htmlFor='descriptionText'>Description</label><br />
+
+                    <textarea 
+                    name='descriptionText'
+                    value={this.state.descriptionText}
+                    onChange={this.handleOnChange}
+                    />
+                    <br /><br />
+
+                    <button type='submit'>Add Lobby</button>
+                </form>
+            </div>
+         );
     }
 }
  
