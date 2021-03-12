be5cf05abeb6a5026b65ec7e5f1a4e8e2f0c6062
branmar97 <brandonmar97@gmail.com>
2021-03-11 19:13:25 -0500

    Refactored get lobby action

--- src/actions/lobbyActions.js
+++ src/actions/lobbyActions.js
@@ -11,17 +11,8 @@ export const fetchLobbies = () => {
     }
 } 
 
-export const fetchLobby = (id) => {
-  return dispatch => {
-    dispatch({ type: 'LOADING_LOBBIES'})
-    fetch(`http://localhost:3000/lobbies/${id}`)
-    .then(response => {
-      return response.json()
-    })
-    .then(responseJSON => {
-      dispatch({ type: 'GET_LOBBY', lobby: responseJSON })
-    })
-  }
+export const getLobby = (id) => {
+  return dispatch({ type: 'GET_LOBBY', id})
 }
 
 export const addLobby = (data) => {
