5cdd8182f9b51a8f9b03fb0f40e3a4c0f24c6ce4
branmar97 <brandonmar97@gmail.com>
2021-03-18 17:31:24 -0400

    Added background image with styling

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Home from './components/Home';
 import About from './components/About';
 import Lobby from './components/Lobby'
 import LobbiesContainer from './containers/LobbiesContainer';
+
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Nav />
-        <Route exact path="/" component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path="/lobbies" component={LobbiesContainer} />
-        <Route path="/lobbies/:id" component={Lobby} />
+      <div className='bg-wraith bg-center bg-top bg-cover bg-no-repeat'>
+        <div className='bg-red-900 bg-opacity-50'>
+          <div className='bg-black bg-opacity-50'>
+            <Nav />
+            <Route exact path="/" component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path="/lobbies" component={LobbiesContainer} />
+            <Route path="/lobbies/:id" component={Lobby} />
+          </div>
+        </div>
       </div>
     </Router>
   );
