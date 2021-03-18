ae5a4605d350ca7323c9ea10dc91d66eb70eb5c1
branmar97 <brandonmar97@gmail.com>
2021-03-18 17:32:03 -0400

    Updated home component styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return ( 
-        <div>
-            <h2>BUILD YOUR NEXT ELITE SQUAD</h2>
+        <div className='bg-white h-screen flex flex-col
+        justify-center items-center'>
+            
+            {/* <h2 className='text-red-500'>BUILD YOUR NEXT ELITE SQUAD</h2>
             <p>Connect with others and find the best teammates.</p>
 
             <Link to='/about'><button>LEARN MORE</button></Link>
 
-            <h3>RECENT LOBBIES</h3>
+            <h3>RECENT LOBBIES</h3> */}
         </div>
      );
 }
