c84aed76c3cb3e254f3746f3a377c8c39d0909a3
branmar97 <brandonmar97@gmail.com>
2021-03-24 19:41:57 -0400

    Styled request cards

--- src/components/Request.js
+++ src/components/Request.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Request = ({ id, request }) => {
     return ( 
-        <li key={id}>
-            <h4>{request.gamertag}</h4>
-            <p>{request.description}</p>
-        </li>
+        <div className='card shadow-2xl bg-gray-500 bg-opacity-50 text-gray-200 p-10'>
+            <h4 className="float font-semibold text-xl tracking-wider uppercase">{request.gamertag}</h4>
+            <p className="text-sm float float-right">30 min ago</p>
+            <p className="text-sm mt-4 text-white">{request.description}</p>
+        </div>
      );
 }
  
