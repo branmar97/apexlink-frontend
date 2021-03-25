1b7ea9ae1471ece07950ce8bab36310650190415
branmar97 <brandonmar97@gmail.com>
2021-03-24 20:33:54 -0400

    Added state and dropdown component for mobile menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Dropdown from './components/Dropdown';
 import Home from './components/Home';
 import About from './components/About';
 import Lobby from './components/Lobby'
@@ -9,11 +10,17 @@ import LobbiesContainer from './containers/LobbiesContainer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  };
 
   return (
     <Router>
         <div className='bg-wraith bg-center bg-top bg-cover bg-no-repeat bg-fixed pb-16'>
-              <Nav />
+              <Nav toggle={toggle} />
+              <Dropdown isOpen={isOpen} toggle={toggle} />
               <Route exact path="/" component={Home} />
               <Route exact path='/about' component={About} />
               <Route exact path="/lobbies" component={LobbiesContainer} />
