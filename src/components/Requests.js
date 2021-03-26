222780018b8354620cd96b961b9887cc9b8250b2
branmar97 <brandonmar97@gmail.com>
2021-03-25 21:10:25 -0400

    Added requests handler to account for no requests

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -7,12 +7,20 @@ const Requests = ({ requests, lobbyId }) => {
     const requestList = filteredRequests.map(request => {
       return <Request request={request} />
     })
+
+    const handleShowRequests = () => {
+        if (requestList.length === 0) {
+            return <h2 className='text-center text-white mt-6'>There are no requests to display.</h2>
+        } else {
+            return requestList
+        }
+    }
     
     return ( 
         <div className='max-w-6xl mx-auto bg-transparent mt-16 my-32'>
             <h1 className='font-bold text-2xl uppercase mb-6'>Requests ({requestList.length})</h1>
             <div className='grid grid-cols-1 gap-10'>
-                {requestList}
+                {handleShowRequests()}
             </div>
         </div>
      );
