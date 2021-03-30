939c15d13a24ddf8b2e71d626576ffef406d975c
branmar97 <brandonmar97@gmail.com>
2021-03-30 10:35:27 -0400

    Created search component

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            searchTerm: '',
+            searchType: 'all'
+         }
+    }
+    render() { 
+        return ( 
+            <div></div>
+         );
+    }
+}
+ 
+export default Search;
