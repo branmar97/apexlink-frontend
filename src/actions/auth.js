0e0d9c68f387cb3865ea2964765e1c9657f89cfd
branmar97 <brandonmar97@gmail.com>
2021-04-06 14:37:02 -0400

    Added signup user action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -13,3 +13,30 @@ const getToken = () => {
     return localStorage.getItem("token");
   }
 };
+
+export const signupUser = (credentials) => {
+    return (dispatch) => {
+      return fetch("http://localhost:3001/signup", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user: credentials })
+      }).then((res) => {
+        if (res.ok) {
+          setToken(res.headers.get("Authorization"));
+          return res
+            .json()
+            .then((userJson) =>
+              dispatch({ type: AUTHENTICATED, payload: userJson })
+            );
+        } else {
+          return res.json().then((errors) => {
+            dispatch({ type: NOT_AUTHENTICATED });
+            return Promise.reject(errors);
+          });
+        }
+      });
+    };
+  };
