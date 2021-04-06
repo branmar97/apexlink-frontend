0ca7cf234d6b39f70cc78d18fda9707425a1efa6
branmar97 <brandonmar97@gmail.com>
2021-04-06 14:58:32 -0400

    Updated signup component

---
+++ src/components/auth/Signup.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signupUser } from "../../actions/auth";
+
+class Signup extends Component {
+    state = { 
+
+    }
+    render() { 
+        return ( 
+            <form>
+
+            </form>
+        );
+    }
+}
+ 
+export default Signup;
