97f74fbce062b3669e4fde01e87b01fa3b09905a
branmar97 <brandonmar97@gmail.com>
2021-04-06 14:34:39 -0400

    Added redux dev tools function to create store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
 import rootReducer from '../reducers/combinedReducers';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,  composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
