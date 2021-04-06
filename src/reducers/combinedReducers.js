b0e66e0fb76027a0a5a20b239b6b6d0210962945
branmar97 <brandonmar97@gmail.com>
2021-04-06 14:42:00 -0400

    Created login user action

--- src/reducers/combinedReducers.js
+++ src/reducers/combinedReducers.js
@@ -1,11 +1,12 @@
 import { combineReducers } from "redux";
 import manageLobbies from './lobbiesReducer';
 import manageRequests from './requestsReducer';
+import authReducer from './auth';
 
 const rootReducer = combineReducers({
     lobbies: manageLobbies,
     requests: manageRequests,
-    auth: (state, action) => 'testing'
+    auth: authReducer
 });
 
 export default rootReducer;
