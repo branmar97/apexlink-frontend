116540db58e04c3e9bc7a1e12dd07b4f1690b2aa
branmar97 <brandonmar97@gmail.com>
2021-04-07 19:01:32 -0400

    Created signup component form

---
+++ src/components/auth/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/auth";
+
+class Login extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <form>
+                
+            </form>
+         );
+    }
+}
+ 
+export default Login;
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -7,15 +7,109 @@ class Signup extends Component {
         email: '',
         password: '',
         gamertag: '',
+        platform: '',
         errors: {status: {message: ''}}
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { email, password } = this.state;
+        this.props
+            .dispatchSignupUser({ email, password })
+            .then(() => this.props.history.push("/"))
+            .catch((errors) => this.setState({ errors }));
+    };
+
     render() { 
         return ( 
-            <form>
+        <form
+            onSubmit={this.handleSubmit}
+            className=''
+        >
+            <h1 className=''>Sign Up</h1>
+            <p className=''>{this.state.errors.status.message}</p>
 
-            </form>
+            <fieldset>
+            <label className='' htmlFor='email'>
+                Email:
+            </label>
+            <input
+                type='text'
+                name='email'
+                id='email'
+                className=''
+                onChange={this.handleChange}
+                value={this.state.email}
+            />
+            </fieldset>
+
+            <fieldset>
+            <label className='' htmlFor='password'>
+                Password:
+            </label>
+            <input
+                type='password'
+                name='password'
+                id='password'
+                className=''
+                onChange={this.handleChange}
+                value={this.state.password}
+            />
+            </fieldset>
+
+            <fieldset>
+            <label className='' htmlFor='gamertag'>
+                Name:
+            </label>
+            <input
+                type='text'
+                name='gamertag'
+                id='gamertag'
+                className=''
+                onChange={this.handleChange}
+                value={this.state.gamertag}
+            />
+            </fieldset>
+
+            <fieldset>
+            <label className='' htmlFor='platform'>
+                Platform:
+            </label>
+            <select 
+                className=''
+                name='platform'
+                defaultValue=''
+                onChange={this.handleChange}
+                >
+                    <option disabled value=''> 
+                        Select an Option 
+                    </option> 
+                    <option value='PC'>PC</option>
+                    <option value='Xbox'>Xbox</option>
+                    <option value='Playstation'>Playstation</option>
+            </select>
+            </fieldset>
+
+            <input
+            className=''
+            type='submit'
+            value='Sign Up'
+            />
+        </form>
         );
     }
 }
- 
-export default Signup;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+      dispatchSignupUser: (credentials) => dispatch(signupUser(credentials))
+    };
+  };
+  
+export default connect(null, mapDispatchToProps)(Signup);
