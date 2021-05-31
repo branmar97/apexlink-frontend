cbe4b0874b026c6cc77b1be04d8983a8446354b9
branmar97 <brandonmar97@gmail.com>
2021-05-31 17:27:57 -0400

    change lobbies container to functional component

--- src/containers/LobbiesContainer.js
+++ src/containers/LobbiesContainer.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ReactLoading from 'react-loading';
 import { connect } from 'react-redux';
 import { fetchLobbies, addLobby } from '../actions/lobbyActions';
 import Search from '../components/Search';
 import LobbyForm from '../components/LobbyForm';
 
-class LobbiesContainer extends Component {
-    componentDidMount() {
-        this.props.fetchLobbies()
-    }    
+const LobbiesContainer = ({ fetchLobbies, addLobby, lobbies, loading }) => {
+    useEffect(() => {
+        fetchLobbies()
+    }, [])
 
-    render() { 
-        if (this.props.loading) {
-            return (
-                <div className='bg-transparent h-screen max-w-6xl justify-center flex flex-col mx-auto
-                items-center content-center font-heebo'>
-                    <ReactLoading type={'spin'} color={'#ef4445'} height={200} width={100} className={'align-center'} />
-                    <h1 className='text-xl sm:text-2xl text-white text-center uppercase mb-4'>Don't worry, this shouldn't take long</h1>
-                </div>
-            );
-        } else {
-            return ( 
-                <div className='bg-transparent 
-                 font-heebo text-white my-32'>
-                    <LobbyForm addLobby={this.props.addLobby} />
-                    <Search lobbies={this.props.lobbies} />
-                </div>
-            );
-        }
+    if (loading) {
+        return (
+            <div className='bg-transparent h-screen max-w-6xl justify-center flex flex-col mx-auto
+            items-center content-center font-heebo'>
+                <ReactLoading type={'spin'} color={'#ef4445'} height={200} width={100} className={'align-center'} />
+                <h1 className='text-xl sm:text-2xl text-white text-center uppercase mb-4'>Don't worry, this shouldn't take long</h1>
+            </div>
+        );
+    } else {
+        return ( 
+            <div className='bg-transparent 
+             font-heebo text-white my-32'>
+                <LobbyForm addLobby={addLobby} />
+                <Search lobbies={lobbies} />
+            </div>
+        );
     }
 }
 
