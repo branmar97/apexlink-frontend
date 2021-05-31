92524d7a47182d46363b2042cb59a499cb1fae21
branmar97 <brandonmar97@gmail.com>
2021-05-31 16:57:29 -0400

    change lobby form to functional component

--- src/components/LobbyForm.js
+++ src/components/LobbyForm.js
@@ -1,132 +1,126 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 
-class LobbyForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            gamemodeText: '',
-            descriptionText: '',
+const LobbyForm = ({ addLobby, currentUser }) => {
+    const [formData, setFormData] = useState(
+        {
+            gamemodeText: "",
+            descriptionText: "",
+            skillText: "",
             micBool: false,
-            skillText: '',
-            hidden: true
+            hidden: true,
         }
+    )
+
+    const handleOnChange = event => {
+        setFormData({...formData, [event.target.name]: event.target.value })
     }
 
-    handleOnChange = event => {
-        this.setState({
-          [event.target.name]: event.target.value
-        })
-    }
-
-    handleOnSubmit = event => {
+    const handleOnSubmit = event => {
         event.preventDefault()
         const lobby = {
-            user_id: this.props.currentUser.id,
-            gamertag: this.props.currentUser.gamertag,
-            platform: this.props.currentUser.platform,
-            region: this.props.currentUser.region,
-            gamemode: this.state.gamemodeText,
-            description: this.state.descriptionText,
-            mic_required: this.state.micBool,
-            skill_level: this.state.skillText
+            user_id: currentUser.id,
+            gamertag: currentUser.gamertag,
+            platform: currentUser.platform,
+            region: currentUser.region,
+            gamemode: formData.gamemodeText,
+            description: formData.descriptionText,
+            mic_required: formData.micBool,
+            skill_level: formData.skillText
         }
-        this.props.addLobby(lobby)
-
+        addLobby(lobby);
         event.target.reset()
 
-        this.setState({
-            descriptionText: '',
-            micBool: false,
-        })
+        setFormData({...formData, descriptionText: "", micBool: false })
     }
 
-    handleCheckbox = () => {
-        const micState = this.state.micBool;
+    const handleCheckbox = () => {
+        const micState = formData.micBool;
         
         if(micState === false) {
-            this.setState({
+            setFormData({
+                ...formData,
                 micBool: true
             });
         }
         else {
-            this.setState({
+            setFormData({
+                ...formData,
                 micBool: false
             });
         }
     }
 
-    handleShowForm = () => {
-        this.setState({
-            hidden: !this.state.hidden
+    const handleShowForm = () => {
+        setFormData({
+            ...formData,
+            hidden: !formData.hidden
         })
     }
 
-    render() { 
-        return ( 
-            <div className='px-4 max-w-6xl mx-auto'>
-                <button className='bg-transparent hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase float-right' onClick={this.handleShowForm}>{this.state.hidden ? 'Create Lobby' : 'Hide Form'}</button>
-                <form className={this.state.hidden ? 'hidden' : ''} onSubmit={this.handleOnSubmit}>
-                    <h1 className='font-bold text-2xl uppercase mb-6'>Create Lobby</h1>
+    return ( 
+        <div className='px-4 max-w-6xl mx-auto'>
+            <button className='bg-transparent hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase float-right' onClick={this.handleShowForm}>{this.state.hidden ? 'Create Lobby' : 'Hide Form'}</button>
+            <form className={formData.hidden ? 'hidden' : ''} onSubmit={handleOnSubmit}>
+                <h1 className='font-bold text-2xl uppercase mb-6'>Create Lobby</h1>
 
-                    <label htmlFor='gamemodeText'>Gamemode</label>
+                <label htmlFor='gamemodeText'>Gamemode</label>
 
-                    <select 
-                    className='border border-gray-400 block py-2 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
-                    name='gamemodeText'
-                    defaultValue=''
-                    onChange={this.handleOnChange}
-                    >
-                        <option disabled value=''> 
-                            Select an Option 
-                        </option> 
-                        <option value='Duos'>Duos</option>
-                        <option value='Trios'>Trios</option>
-                        <option value='Ranked'>Ranked</option>
-                    </select>
+                <select 
+                className='border border-gray-400 block py-2 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
+                name='gamemodeText'
+                defaultValue=''
+                onChange={handleOnChange}
+                >
+                    <option disabled value=''> 
+                        Select an Option 
+                    </option> 
+                    <option value='Duos'>Duos</option>
+                    <option value='Trios'>Trios</option>
+                    <option value='Ranked'>Ranked</option>
+                </select>
 
-                    <label htmlFor='skillText'>Skill Level</label>
+                <label htmlFor='skillText'>Skill Level</label>
 
-                    <select 
-                    className='border border-gray-400 block py-2 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
-                    name='skillText'
-                    defaultValue=''
-                    onChange={this.handleOnChange}
-                    >
-                        <option disabled value=''> 
-                            Select an Option 
-                        </option> 
-                        <option value='Beginner'>Beginner</option>
-                        <option value='Intermediate'>Intermediate</option>
-                        <option value='Experienced'>Experienced</option>
-                    </select>
+                <select 
+                className='border border-gray-400 block py-2 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
+                name='skillText'
+                defaultValue=''
+                onChange={handleOnChange}
+                >
+                    <option disabled value=''> 
+                        Select an Option 
+                    </option> 
+                    <option value='Beginner'>Beginner</option>
+                    <option value='Intermediate'>Intermediate</option>
+                    <option value='Experienced'>Experienced</option>
+                </select>
 
-                    <div className='pb-4'>
-                        <label className='mr-4' htmlFor='micBool'>Mic required?</label>
+                <div className='pb-4'>
+                    <label className='mr-4' htmlFor='micBool'>Mic required?</label>
 
-                        <input
-                        type='checkbox'
-                        name='micBool'
-                        defaultChecked={this.state.micBool}
-                        onClick={this.handleCheckbox}
-                        />
-                    </div>
-
-                    <label htmlFor='descriptionText'>Description</label>
-
-                    <textarea 
-                    className='border border-gray-400 block py-6 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
-                    name='descriptionText'
-                    placeholder='Some details about your lobby'
-                    value={this.state.descriptionText}
-                    onChange={this.handleOnChange}
+                    <input
+                    type='checkbox'
+                    name='micBool'
+                    defaultChecked={formData.micBool}
+                    onClick={handleCheckbox}
                     />
+                </div>
 
-                    <button className="bg-transparent hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase mt-10" type='submit'>Add Lobby</button>
-                </form>
-            </div>
-         );
-    }
+                <label htmlFor='descriptionText'>Description</label>
+
+                <textarea 
+                className='border border-gray-400 block py-6 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
+                name='descriptionText'
+                placeholder='Some details about your lobby'
+                value={formData.descriptionText}
+                onChange={handleOnChange}
+                />
+
+                <button className="bg-transparent hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase mt-10" type='submit'>Add Lobby</button>
+            </form>
+        </div>
+    );
 }
 
 const mapStateToProps = ({ auth: { currentUser } }) => {
