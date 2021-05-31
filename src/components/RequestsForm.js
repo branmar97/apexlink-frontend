9e9925a249ff3c74706f73e446d8a0a1bbf7b28e
branmar97 <brandonmar97@gmail.com>
2021-05-31 17:07:52 -0400

    change requests form to functional component

--- src/components/RequestsForm.js
+++ src/components/RequestsForm.js
@@ -1,56 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
+import { addLobby } from '../actions/lobbyActions';
 
-class RequestsForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            descriptionText: ''
-         }
+const RequestsForm = ({ currentUser, lobbyId, addRequest}) => {
+    const [input, setInput] = useState("")
+
+    const handleOnChange = event => {
+        setInput(event.target.value)
     }
 
-    handleOnChange = event => {
-        this.setState({
-          [event.target.name]: event.target.value
-        })
-    }
-
-    handleOnSubmit = event => {
+    const handleOnSubmit = event => {
         event.preventDefault()
         const request = {
-            user_id: this.props.currentUser.id,
-            gamertag: this.props.currentUser.gamertag,
-            description: this.state.descriptionText,
-            lobby_id: this.props.lobbyId
+            user_id: currentUser.id,
+            gamertag: currentUser.gamertag,
+            description: input,
+            lobby_id: lobbyId
         }
-        this.props.addRequest(request)
-
-        this.setState({
-            descriptionText: ''
-        })
+        addRequest(request)
+        setInput("")
     }
 
-    render() { 
-        return ( 
-            <div>
-                <form className='mx-auto' onSubmit={this.handleOnSubmit}>
-                    <h2 className='font-bold text-2xl uppercase mb-6'>Request to Join</h2>
+    return ( 
+        <div>
+            <form className='mx-auto' onSubmit={handleOnSubmit}>
+                <h2 className='font-bold text-2xl uppercase mb-6'>Request to Join</h2>
 
-                    <label htmlFor='descriptionText'>Description</label>
+                <label htmlFor='descriptionText'>Description</label>
 
-                    <textarea 
-                        className='border border-gray-400 block py-6 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
-                        name='descriptionText'
-                        placeholder='Some details about your request'
-                        value={this.state.descriptionText}
-                        onChange={this.handleOnChange}
-                    />
+                <textarea 
+                    className='border border-gray-400 block py-6 px-4 w-full focus:outline-none focus:border-red-500 text-black mb-6'
+                    name='description'
+                    placeholder='Some details about your request'
+                    value={input}
+                    onChange={handleOnChange}
+                />
 
-                    <button className='bg-transparent hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase mt-10' type='submit'>Join</button>
-                </form>
-            </div>
-         );
-    }
+                <button className='bg-transparent hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase mt-10' type='submit'>Join</button>
+            </form>
+        </div>
+     );
 }
 
 const mapStateToProps = ({ auth: { currentUser } }) => {
