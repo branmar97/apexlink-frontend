4b27bfc516e2d42b76c05b75fb79041a71163c58
branmar97 <brandonmar97@gmail.com>
2021-05-31 17:24:12 -0400

    change search to functional component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,143 +1,142 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Lobbies from './Lobbies';
 
-class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            searchTerm: '',
-            searchPlatform: 'all',
-            searchGamemode: 'all',
-            searchRegion: 'all'
-         }
-    }
+const Search = ({ lobbies }) => {
+    const [formData, setFormData] = useState(
+        {
+            searchTerm: "",
+            searchPlatform: "all",
+            searchGamemode: "all",
+            searchRegion: "all"
+        }
+    )
 
-    handleOnChange = (event) => {
-        this.setState({
+    const handleOnChange = event => {
+        setFormData({
+            ...formData,
             [event.target.name]: event.target.value
         })
     }
 
-    nameFilter = (lobby) => {
-        if (this.state.searchTerm === '') {
+    const nameFilter = lobby => {
+        if (formData.searchTerm === '') {
             return lobby 
-        } else if (lobby.gamertag.toLowerCase().includes(this.state.searchTerm.toLowerCase())) {
+        } else if (lobby.gamertag.toLowerCase().includes(formData.searchTerm.toLowerCase())) {
             return lobby
         }
     }
 
-    regionFilter = (lobby) => {
-        if (this.state.searchRegion === 'all') {
+    const regionFilter = lobby => {
+        if (formData.searchRegion === 'all') {
             return lobby 
-        } else if (lobby.region.toLowerCase().includes(this.state.searchRegion.toLowerCase())) {
-            return lobby 
-        }
-    }
-
-    platformFilter = (lobby) => {
-        if (this.state.searchPlatform === 'all') {
-            return lobby 
-        } else if (lobby.platform.toLowerCase().includes(this.state.searchPlatform.toLowerCase())) {
+        } else if (lobby.region.toLowerCase().includes(formData.searchRegion.toLowerCase())) {
             return lobby 
         }
     }
 
-    gamemodeFilter = (lobby) => {
-        if (this.state.searchGamemode === 'all') {
+    const platformFilter = lobby => {
+        if (formData.searchPlatform === 'all') {
             return lobby 
-        } else if (lobby.gamemode.toLowerCase().includes(this.state.searchGamemode.toLowerCase())) {
+        } else if (lobby.platform.toLowerCase().includes(formData.searchPlatform.toLowerCase())) {
             return lobby 
         }
     }
 
-    filteredLobbies = () => {
-        return this.props.lobbies.filter(this.nameFilter).filter(this.regionFilter).filter(this.platformFilter).filter(this.gamemodeFilter)
+    const gamemodeFilter = lobby => {
+        if (formData.searchGamemode === 'all') {
+            return lobby 
+        } else if (lobby.gamemode.toLowerCase().includes(formData.searchGamemode.toLowerCase())) {
+            return lobby 
+        }
     }
 
-    render() { 
-        const renderLobbies = () => {
-            if (this.filteredLobbies().length > 0) {
-                return (
-                    <Lobbies lobbies={this.filteredLobbies()} />
-                )
-            } else {
-                return (
-                    <div>
-                        <h2 className='text-center text-white h-screen px-4 max-w-6xl mx-auto bg-transparent mt-16'>There are no lobbies to display.</h2>
+    const filteredLobbies = () => {
+        return lobbies.filter(nameFilter).filter(regionFilter).filter(platformFilter).filter(gamemodeFilter)
+    }
+
+    const renderLobbies = () => {
+        if (filteredLobbies().length > 0) {
+            return (
+                <Lobbies lobbies={filteredLobbies()} />
+            )
+        } else {
+            return (
+                <div>
+                    <h2 className='text-center text-white h-screen px-4 max-w-6xl mx-auto bg-transparent mt-16'>There are no lobbies to display.</h2>
+                </div>
+            )
+        }
+    }
+
+    return ( 
+        <div className='px-4 max-w-6xl mx-auto mt-16 bg-transparent'>
+            <h2 className='font-bold text-2xl uppercase mb-6'>Browse Lobbies</h2>
+
+            <form>
+                <div className="flex flex-wrap -mx-4 mb-2">
+                    <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                        <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Name</label>
+
+                        <input 
+                        className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                        type='text' 
+                        name='searchTerm' 
+                        value={formData.searchTerm} 
+                        placeholder='Search...' 
+                        onChange={handleOnChange} />
                     </div>
-                )
-            }
-        }
-        return ( 
-            <div className='px-4 max-w-6xl mx-auto mt-16 bg-transparent'>
-                <h2 className='font-bold text-2xl uppercase mb-6'>Browse Lobbies</h2>
 
-                <form>
-                    <div className="flex flex-wrap -mx-4 mb-2">
-                        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                            <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Name</label>
-
-                            <input 
-                            className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                            type='text' 
-                            name='searchTerm' 
-                            value={this.state.searchTerm} 
-                            placeholder='Search...' 
-                            onChange={this.handleOnChange} />
-                        </div>
-
-                        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                            <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Region</label>
-                            <div className="relative">
-                                <select 
-                                className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                                name='searchRegion'
-                                onChange={this.handleOnChange}
-                                >
-                                    <option value='all'>All</option>
-                                    <option value='united states'>United States</option>
-                                    <option value='canada'>Canada</option>
-                                    <option value='united kingdom'>United Kingdom</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                            <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Platform</label>
-                            
+                    <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                        <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Region</label>
+                        <div className="relative">
                             <select 
                             className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                            name='searchPlatform'
-                            onChange={this.handleOnChange}
+                            name='searchRegion'
+                            onChange={handleOnChange}
                             >
                                 <option value='all'>All</option>
-                                <option value='pc'>PC</option>
-                                <option value='xbox'>Xbox</option>
-                                <option value='playstation'>Playstation</option>
-                            </select>
-                        </div>
-
-                        <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
-                            <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Gamemode</label>
-                            
-                            <select 
-                            className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                            name='searchGamemode'
-                            onChange={this.handleOnChange}
-                            >
-                                <option value='all'>All</option>
-                                <option value='duos'>Duos</option>
-                                <option value='trios'>Trios</option>
-                                <option value='ranked'>Ranked</option>
+                                <option value='united states'>United States</option>
+                                <option value='canada'>Canada</option>
+                                <option value='united kingdom'>United Kingdom</option>
                             </select>
                         </div>
                     </div>
-                </form>
-                
-                {renderLobbies()}
-            </div>
-         );
-    }
+
+                    <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                        <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Platform</label>
+                        
+                        <select 
+                        className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                        name='searchPlatform'
+                        onChange={handleOnChange}
+                        >
+                            <option value='all'>All</option>
+                            <option value='pc'>PC</option>
+                            <option value='xbox'>Xbox</option>
+                            <option value='playstation'>Playstation</option>
+                        </select>
+                    </div>
+
+                    <div className="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                        <label className='block uppercase tracking-wide text-white text-xs font-bold mb-2' htmlFor='regionText'>Gamemode</label>
+                        
+                        <select 
+                        className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                        name='searchGamemode'
+                        onChange={handleOnChange}
+                        >
+                            <option value='all'>All</option>
+                            <option value='duos'>Duos</option>
+                            <option value='trios'>Trios</option>
+                            <option value='ranked'>Ranked</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+            
+            {renderLobbies()}
+        </div>
+    );
 }
  
 export default Search;
