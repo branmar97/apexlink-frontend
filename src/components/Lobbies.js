4acc46457af399198a4e785f63b6244488c6aef9
branmar97 <brandonmar97@gmail.com>
2021-05-31 15:04:54 -0400

    remove component import from lobbies

--- src/components/Lobbies.js
+++ src/components/Lobbies.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import LobbyCard from './LobbyCard';
 import { connect } from 'react-redux';
 import { fetchRequests } from '../actions/requestActions';
