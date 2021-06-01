0c8883711d9a6c6fb49aa68e5418fa7932e56093
branmar97 <brandonmar97@gmail.com>
2021-06-01 09:04:22 -0400

    update lobby props and add loading spinner

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react';
 import RequestsContainer from '../containers/RequestsContainer';
+import ReactLoading from 'react-loading';
 import { connect } from 'react-redux';
 import { getLobby } from '../actions/lobbyActions';
 import { Link } from 'react-router-dom'
 import TimeAgo from 'timeago-react';
 
-const Lobby = ({ lobby, getLobby, match }) => {
+const Lobby = ({ lobby, getLobby, match, loading }) => {
     useEffect(() => {
         getLobby(match.params.id);
     }, [])
-
+    console.log(lobby.data)
     const micRequired = () => {
-        if (lobby.mic_required) {
+        if (lobby.data.mic_required) {
             return (
                 <svg className="w-6 h-6 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clipRule="evenodd" /></svg>
             )
@@ -21,11 +22,11 @@ const Lobby = ({ lobby, getLobby, match }) => {
     }
 
     const platformType = () => {
-        if (lobby.platform === "Xbox") {
+        if (lobby.data.platform === "Xbox") {
             return (
                 <img alt='Xbox Icon' src="https://img.icons8.com/fluent/24/000000/xbox.png"/>
             )
-        } else if (lobby.platform === "Playstation") {
+        } else if (lobby.data.platform === "Playstation") {
             return (
                 <img alt='Playstation Icon' src="https://img.icons8.com/color/24/000000/play-station.png"/>
             )
@@ -35,35 +36,46 @@ const Lobby = ({ lobby, getLobby, match }) => {
             )
         }
     }
-
-    return (
-        <div className='bg-transparent font-heebo text-white my-32 px-4 max-w-6xl mx-auto'>
-            <Link to='/lobbies'>
-                <span className='flex mb-4'>
-                    <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                    </svg>
-                    <p className='mt-0.5 ml-2 text-xl'>Back</p>
-                </span>
-            </Link>
-
-            <div className='shadow-2xl bg-gray-500 bg-opacity-50 text-gray-200 p-10'>  
-                <p className="text-sm mb-6 mt-2 float-right">Lobby #{lobby.id}</p>
-                <div className='flex'>
-                    <h2 className="font-semibold text-4xl tracking-wider uppercase mr-4">{lobby.gamertag}</h2>
-                    <img alt='Host Icon' src="https://img.icons8.com/android/40/ffffff/crown.png"/>
+    
+    if (loading || !lobby.data) {
+        return (
+            <div className='bg-transparent h-screen max-w-6xl justify-center flex flex-col mx-auto
+            items-center content-center font-heebo'>
+                <ReactLoading type={'spin'} color={'#ef4445'} height={200} width={100} className={'align-center'} />
+                <h1 className='text-xl sm:text-2xl text-white text-center uppercase mb-4'>Don't worry, this shouldn't take long</h1>
+            </div>
+        )
+    } else {
+        return (
+            <div className='bg-transparent font-heebo text-white my-32 px-4 max-w-6xl mx-auto'>
+                <Link to='/lobbies'>
+                    <span className='flex mb-4'>
+                        <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                        </svg>
+                        <p className='mt-0.5 ml-2 text-xl'>Back</p>
+                    </span>
+                </Link>
+    
+                <div className='shadow-2xl bg-gray-500 bg-opacity-50 text-gray-200 p-10'>  
+                    <p className="text-sm mb-6 mt-2 float-right">Lobby #{lobby.data.id}</p>
+                    <div className='flex'>
+                        <h2 className="font-semibold text-4xl tracking-wider uppercase mr-4">{lobby.user.gamertag}</h2>
+                        <img alt='Host Icon' src="https://img.icons8.com/android/40/ffffff/crown.png"/>
+                    </div>
+                    <p className="text-sm mt-4">{lobby.data.gamemode} • {<TimeAgo datetime={lobby.data.created_at}
+                                                                                        locale='en'
+                                                                                                />}</p>
+                    <div className="flex mt-2">{platformType()}{micRequired()}</div>
+                    <p className='text-sm mt-2'>{lobby.data.region} • {lobby.data.skill_level}</p>
+                    <p className="text-sm mt-4 text-white">{lobby.data.description}</p>
                 </div>
-                <p className="text-sm mt-4">{lobby.gamemode} • {<TimeAgo datetime={lobby.created_at}
-                                                                                    locale='en'
-                                                                                            />}</p>
-                <div className="flex mt-2">{platformType()}{micRequired()}</div>
-                <p className='text-sm mt-2'>{lobby.region} • {lobby.skill_level}</p>
-                <p className="text-sm mt-4 text-white">{lobby.description}</p>
+                <div>
+                    <RequestsContainer lobbyId={lobby.data.id} />
+                </div>
             </div>
-            <div>
-                <RequestsContainer lobbyId={lobby.id} />
-            </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 const mapStateToProps = state => {
