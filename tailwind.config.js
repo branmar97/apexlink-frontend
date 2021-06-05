fa62ae37037f70a54c34b270b47842c49df79a4b
branmar97 <brandonmar97@gmail.com>
2021-06-05 18:37:31 -0400

    remove duke font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
         'wraith': "url('/src/images/site-background-blurred.png')"
       },
       fontFamily: {
-        'duke': ["Duke"],
         'heebo': "url('https://fonts.googleapis.com/css2?family=Heebo&display=swap')"
       }
     },
