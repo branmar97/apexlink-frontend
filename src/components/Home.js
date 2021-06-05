4776a21562d27fbd8bbfe5a4a1803a5a85e84859
branmar97 <brandonmar97@gmail.com>
2021-06-05 18:37:51 -0400

    change duke font to heebo

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className='bg-transparent h-screen flex flex-col
         justify-center items-center font-heebo'>
 
-            <h1 className='text-9xl sm:text-12xl text-red-500 uppercase text-center mb-4 font-duke tracking-widest'>Apex<span className='text-white'>Link</span></h1>
+            <h1 className='text-9xl sm:text-12xl text-red-500 uppercase text-center mb-4 tracking-widest'>Apex<span className='text-white'>Link</span></h1>
             <h1 className='text-2xl sm:text-4xl text-white text-center mb-4'>Build an Elite Apex Legends Squad</h1>
 
             <Link to='/lobbies'><button className="bg-transparent hover:bg-red-500 text-white hover:text-white py-4 px-8 border border-white hover:border-transparent uppercase mt-10">Get Started</button></Link>
