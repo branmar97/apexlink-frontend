087e765ad1275a369558b9dcc146cb04ab110c26
branmar97 <brandonmar97@gmail.com>
2022-04-21 10:25:03 -0400

    refactor live status functions

--- src/containers/LobbyContainer.js
+++ src/containers/LobbyContainer.js
@@ -2,28 +2,25 @@ import React, { useEffect } from 'react';
 import RequestsContainer from './RequestsContainer';
 import ReactLoading from 'react-loading';
 import { connect } from 'react-redux';
-import { getLobby, updateLobby } from '../actions/lobbyActions';
+import { getLobby, updateLobbyStatus } from '../actions/lobbyActions';
 import { Link } from 'react-router-dom';
 import isObjNotEmpty from '../helpers';
 import TimeAgo from 'timeago-react';
 
-const LobbyContainer = ({ lobby, getLobby, updateLobby, currentUser, match, loading }) => {
+const LobbyContainer = ({ lobby, getLobby, updateLobbyStatus, currentUser, match, loading }) => {
     useEffect(() => {
         getLobby(match.params.id);
     }, [])
 
-    const endSessionHandler = () => {
-        const newLobby = {
-            ...lobby,
-            live: false
-        }
-        updateLobby(newLobby);
+    const endSessionHandler = event => {
+        event.preventDefault();
+        updateLobbyStatus(lobby.id);
     }
 
     const updateLiveStatus = () => {
         if (lobby.user.id == currentUser.id && lobby.live) {
             return (
-                <form onSubmit={endSessionHandler()}>
+                <form onSubmit={endSessionHandler}>
                     <button className="hover:bg-red-500 text-white hover:text-white py-2 px-6 border border-white hover:border-transparent uppercase" type="submit">End Session</button>
                 </form>
             )
@@ -43,11 +40,15 @@ const LobbyContainer = ({ lobby, getLobby, updateLobby, currentUser, match, load
     const liveStatus = () => {
         if (lobby.live) {
             return (
-                <svg className="animate-ping w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" /></svg>
+                <div className='flex float-right text-green-600 ml-10 text-md'>
+                    <svg className="animate-ping w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" /></svg> <p className="ml-2">LIVE</p>
+                </div>
             )
         } else {
             return (
-                <svg className="text-red-600 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414" /></svg>
+                <div className='flex float-right text-green-600 ml-10 text-md'>
+                    <svg className="text-red-600 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414" /></svg>
+                </div>
             )
         }
     }
@@ -63,7 +64,7 @@ const LobbyContainer = ({ lobby, getLobby, updateLobby, currentUser, match, load
             )
         } else {
             return (
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg> 
             )
         }
     }
@@ -90,9 +91,7 @@ const LobbyContainer = ({ lobby, getLobby, updateLobby, currentUser, match, load
                 <div className='shadow-2xl bg-gray-500 bg-opacity-50 text-gray-200 p-4 py-6 sm:p-10 flex flex-col overflow-hidden'>  
                     <div>
                         <small className="text-sm mb-2">Lobby #{lobby.id}</small>
-                        <div className='flex float-right text-green-600 ml-10 text-md'>
-                            {liveStatus()} <p className="ml-2">LIVE</p>
-                        </div>
+                        {liveStatus()}
                     </div>
                     <div className='flex'>
                         <h2 className="font-semibold text-4xl tracking-wider uppercase mr-4">{lobby.user.gamertag}</h2>
@@ -130,7 +129,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         getLobby: (id) => dispatch(getLobby(id)),
-        updateLobby: (id) => dispatch(updateLobby(id)),
+        updateLobbyStatus: (id) => dispatch(updateLobbyStatus(id)),
     }
 }
  
