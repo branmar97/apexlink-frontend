fcd2e744f0db5a9cf6390e832e4a7f91c0f4ca5d
branmar97 <brandonmar97@gmail.com>
2022-04-22 08:51:23 -0400

    add check live status

--- src/containers/RequestsContainer.js
+++ src/containers/RequestsContainer.js
@@ -4,21 +4,40 @@ import RequestsForm from './RequestsFormContainer';
 import { connect } from 'react-redux';
 import { fetchRequests } from '../actions/requestActions';
 
-const RequestsContainer = ({ fetchRequests, requests, lobbyId }) => {
+const RequestsContainer = ({ fetchRequests, requests, lobbyId, liveStatus }) => {
     useEffect(() => {
         fetchRequests(lobbyId)
     }, [])
 
+    const checkLiveStatus = () => {
+        if (liveStatus) {
+            return (
+                <div>
+                    <RequestsForm 
+                    lobbyId={lobbyId}
+                    />
+                    <Requests
+                    requests={requests}
+                    lobbyId={lobbyId}
+                    />
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h2 className='text-center text-white px-4 max-w-6xl mx-auto mt-16'>The lobby is offline and no longer accepting join requests.</h2>
+                    <Requests
+                    requests={requests}
+                    lobbyId={lobbyId}
+                    />
+                </div>
+            )
+        }
+    }
+
     return ( 
-        <div className='
-        font-heebo text-white my-32'>
-            <RequestsForm 
-                lobbyId={lobbyId}
-            />
-            <Requests
-                requests={requests}
-                lobbyId={lobbyId}
-            />
+        <div className='font-heebo text-white my-32'>
+            {checkLiveStatus()}
         </div>
      );
 }
