ac26fa86b81d1c30956415d1ed1cc777d70fab1e
branmar97 <brandonmar97@gmail.com>
2022-05-20 10:22:03 -0400

    add profiles reducer to combined reducers

--- src/reducers/combinedReducers.js
+++ src/reducers/combinedReducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import manageLobbies from './lobbiesReducer';
 import manageRequests from './requestsReducer';
+import manageProfiles from './profilesReducer';
 import authReducer from './auth';
 
 const rootReducer = combineReducers({
     lobbies: manageLobbies,
     requests: manageRequests,
+    profiles: manageProfiles,
     auth: authReducer
 });
 
