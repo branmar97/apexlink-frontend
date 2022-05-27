3afef499c6a18e1ddbbab734849112db36e1a553
branmar97 <brandonmar97@gmail.com>
2022-05-27 09:18:47 -0400

    map state and dispatch for profiles

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,6 +1,24 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getProfile } from '../actions/profileActions';
+
 const ProfileContainer = () => {
     return (
         <div></div>
     )
 }
+
+const mapStateToProps = state => {
+    return {
+        currentProfile: state.profiles.current,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getProfile: id => dispatch(getProfile(id)),
+    }
+}
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileContainer);
 
