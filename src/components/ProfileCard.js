c5b5a63302a7e795e61c6fe27a60658f11645d8f
branmar97 <brandonmar97@gmail.com>
2022-06-10 21:56:03 -0400

    render profile card props

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const ProfileCard = () => {
+const ProfileCard = ({ gamertag, bio, link, avatar, platform, region }) => {
     return (
         <div>
-            
+            <h1>{gamertag}</h1>
+            <p>{region}</p>
+            <p>{platform}</p>
+            <p>{link}</p>
+            <p>{bio}</p>
         </div>
     )
 }
